719d00a711efa5644707174837aedd0318510b41
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-11-17 14:27:29 +0900

    vercel test

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,7 +2,7 @@ import AdminHeader from '@app/admin/AdminHeader';
 import UserList from '@app/admin/UserList';
 
 const prefetchUsers = async () => {
-	const res = await fetch(`${process.env.NEXTAUTH_URL}api/user`);
+	const res = await fetch(`${process.env.NEXTAUTH_URL}api/user`, { cache: 'no-store' });
 
 	if (!res.ok) throw new Error(`[${res.status}]${res.statusText}`);
 
