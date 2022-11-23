907914b033e8d88d03151d016e5dbd75d6f883bf
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-11-23 14:23:23 +0900

    vercel 배포 테스트

--- db/config/config.js
+++ db/config/config.js
@@ -1,4 +1,7 @@
 'use strict';
+const fs = require('fs');
+// const rdsCa = fs.readFileSync(__dirname + '/rds-ca-2019-root.pem');
+
 module.exports = {
 	development: {
 		username: process.env.DB_USER,
@@ -21,7 +24,10 @@ module.exports = {
 		host: process.env.DB_HOST,
 		dialect: 'mysql',
 		dialectOptions: {
-			ssl: true,
+			ssl: {
+				rejectUnauthorized: true,
+				ca: 'https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem',
+			},
 		},
 	},
 };
